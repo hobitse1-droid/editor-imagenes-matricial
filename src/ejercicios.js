615b5680c5ba92c98444af9d84a7bb6120581d07
hobitse1-droide <hobitse1@gmail.com>
2025-11-18 23:41:40 -0600

    Ejercicio 4.1 resuelto

--- src/ejercicios.js
+++ src/ejercicios.js
@@ -494,22 +494,35 @@ function rotar90Grados(matriz) {
  * const mezcla = mezclarImagenes(imagen1, imagen2, 0.5); // 50/50
  */
 function mezclarImagenes(matriz1, matriz2, factor) {
-  // TODO: Implementar mezcla de imágenes
-  
   // 1. Verificar que tengan las mismas dimensiones
-  // const dims1 = obtenerDimensiones(matriz1);
-  // const dims2 = obtenerDimensiones(matriz2);
-  // if (dims1.filas !== dims2.filas || dims1.columnas !== dims2.columnas) {
-  //   throw new Error('Las imágenes deben tener el mismo tamaño');
-  // }
-  
-  // 2. Para cada pixel:
-  // r = r1 * (1 - factor) + r2 * factor
-  // g = g1 * (1 - factor) + g2 * factor
-  // b = b1 * (1 - factor) + b2 * factor
-  
-  return []; // REEMPLAZAR
+  const dims1 = obtenerDimensiones(matriz1);
+  const dims2 = obtenerDimensiones(matriz2);
+
+  if (dims1.filas !== dims2.filas || dims1.columnas !== dims2.columnas) {
+    throw new Error('Las imágenes deben tener el mismo tamaño');
+  }
+
+  // 2. Crear matriz resultado
+  const resultado = copiarMatriz(matriz1);
+
+  // 3. Para cada pixel, aplicar la combinación lineal
+  for (let i = 0; i < resultado.length; i++) {
+    for (let j = 0; j < resultado[i].length; j++) {
+      const p1 = matriz1[i][j];
+      const p2 = matriz2[i][j];
+
+      resultado[i][j] = {
+        r: limitarValorColor(p1.r * (1 - factor) + p2.r * factor),
+        g: limitarValorColor(p1.g * (1 - factor) + p2.g * factor),
+        b: limitarValorColor(p1.b * (1 - factor) + p2.b * factor),
+        a: limitarValorColor(p1.a * (1 - factor) + p2.a * factor)
+      };
+    }
+  }
+
+  return resultado;
 }
+
 
 /**
  * Ejercicio 4.2: Filtro Sepia (9 puntos)
